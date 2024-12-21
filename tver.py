2fe77701f13d8a12452684b0792196fc1e66bd35
e0406370 <e0406370@u.nus.edu>
2024-12-21 16:09:32 +0800

    fix: implement explicit wait for episode list
    issue #1

--- tver.py
+++ tver.py
@@ -2,29 +2,42 @@
     Extracts links from a series that is currently streaming on TVer into a text file, using Selenium and Beautiful Soup.
 """
 
-import re, sys, time
+import re, sys
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from bs4 import BeautifulSoup
+
 
 TVER_BASE = "https://tver.jp"
 TVER_FILE = "tver.txt"
 
 
 def make_webdriver():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
 
     opt = Options()
-    opt.add_argument("--headless")  # activates the browser in the background
+    # opt.add_argument("--headless")  # activates the browser in the background
 
     driver = webdriver.Chrome(options=opt)
     return driver
 
 
+def wait_element_visible(driver, by, locator, timeout=10):
+
+    wait = WebDriverWait(driver, timeout)
+    wait.until(EC.visibility_of_element_located((by, locator)))
+
+
 def scrape_tver(link: str):
-    from bs4 import BeautifulSoup
 
     driver = make_webdriver()
     driver.get(link)
-    time.sleep(5)
+
+    wait_element_visible(driver, By.CSS_SELECTOR, "[class^='episode-live-list-column_episodeList']")
 
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
