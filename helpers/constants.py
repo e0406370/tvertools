c8a40749dfaa21ba0fb00d9d6de88d1d1056c264
e0406370 <e0406370@u.nus.edu>
2024-12-21 18:52:07 +0800

    chore: resolve linting issues

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,9 +1,10 @@
 TVER_BASE = "https://tver.jp"
 TVER_FILE = "tver.txt"
 
+
 class ClassNames:
-  LOAD_ICON = "loading_box"
-  SERIES_TITLE = "series-main_title"
-  
-  EPISODE_LIST = "episode-live-list-column_episodeList"
-  EPISODE_ROW = "episode-row_container"
+    LOAD_ICON = "loading_box"
+    SERIES_TITLE = "series-main_title"
+
+    EPISODE_LIST = "episode-live-list-column_episodeList"
+    EPISODE_ROW = "episode-row_container"
--- helpers/locators.py
+++ helpers/locators.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
-
 from helpers.constants import ClassNames
+
 
 load_icon = (By.CSS_SELECTOR, f"[class^='{ClassNames.LOAD_ICON}']")
 episode_list = (By.CSS_SELECTOR, f"[class^='{ClassNames.EPISODE_LIST}']")
