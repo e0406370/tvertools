90e64359cba9ebec627eac1a12c5b43a6fead69b
e0406370 <e0406370@u.nus.edu>
2025-01-05 02:49:41 +0800

    test: render_tver with invalid series and not airing series

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,10 +1,31 @@
+import pytest
 from helpers import *
 from tver import render_tver
 
 
-def test_render_tver_valid_series():
-    
-    with make_webdriver() as driver:
-        render_status = render_tver(driver, Tver.get_series_url(Tver.SERIES_ID["valid"]))
+@pytest.fixture(scope="session")
+def shared_driver():
 
-    assert render_status == True
+    with make_webdriver() as driver:
+        yield driver
+
+
+def test_render_tver_with_valid_series(shared_driver):
+
+    render_status = render_tver(shared_driver, Tver.get_series_url(Tver.SERIES_ID["valid"]))
+
+    assert render_status is True
+
+
+def test_render_tver_with_invalid_series(shared_driver):
+
+    render_status = render_tver(shared_driver, Tver.get_series_url(Tver.SERIES_ID["invalid"]))
+
+    assert render_status is False
+
+
+def test_render_tver_with_not_airing_series(shared_driver):
+
+    render_status = render_tver(shared_driver, Tver.get_series_url(Tver.SERIES_ID["not_airing"]))
+
+    assert render_status is False
