08f1ca9a6cdd3138851af847d944ae584610fcdb
e0406370 <e0406370@u.nus.edu>
2025-01-05 03:13:29 +0800

    test: improve test_render with stdout checking

--- tests/test_render.py
+++ tests/test_render.py
@@ -12,20 +12,25 @@ def shared_driver():
 
 def test_render_tver_with_valid_series(shared_driver):
 
-    render_status = render_tver(shared_driver, Tver.get_series_url(Tver.SERIES_ID["valid"]))
+    series_url = Tver.get_series_url(Tver.SERIES_ID["valid"])
+    render_status = render_tver(shared_driver, series_url)
 
-    assert render_status is True
-
-
-def test_render_tver_with_invalid_series(shared_driver):
-
-    render_status = render_tver(shared_driver, Tver.get_series_url(Tver.SERIES_ID["invalid"]))
-
-    assert render_status is False
+    assert render_status is True, "render_tver should return True for valid series"
 
 
-def test_render_tver_with_not_airing_series(shared_driver):
+def test_render_tver_with_invalid_series(shared_driver, capsys):
 
-    render_status = render_tver(shared_driver, Tver.get_series_url(Tver.SERIES_ID["not_airing"]))
+    series_url = Tver.get_series_url(Tver.SERIES_ID["invalid"])
+    render_status = render_tver(shared_driver, series_url)
 
-    assert render_status is False
+    assert Messages.ERROR_INVALID_SERIES_ID in capsys.readouterr().out, "Expected error message for invalid series"
+    assert render_status is False, "render_tver should return False for invalid series"
+
+
+def test_render_tver_with_not_airing_series(shared_driver, capsys):
+
+    series_url = Tver.get_series_url(Tver.SERIES_ID["not_airing"])
+    render_status = render_tver(shared_driver, series_url)
+
+    assert Messages.ERROR_NOT_AIRING_SERIES in capsys.readouterr().out, "Expected error message for not airing series"
+    assert render_status is False, "render_tver should return False for not airing series"
