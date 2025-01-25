9dcefcddbdadd965f8ead8fffc9ff229e8c6271b
e0406370 <e0403670@u.nus.edu>
2025-01-25 10:16:09 +0000

    [auto] Update details in TEST_EPISODE
    Triggered by: schedule, Job: run-update-script, Run ID: 12964061815, Run Number: 15

--- helpers/constants.py
+++ helpers/constants.py
@@ -10,10 +10,10 @@ class Tver:
     
     TEST_EPISODE = {
         "valid": {
-            "id": "ep5h16tcyx",
-            "title": "#1150「怪盗キッドと王冠マジック（前編）」",
-            "broadcast": "1月18日(土)放送分",
-            "end": "1月25日(土)17:59 終了予定"
+            "id": "epziu17xe8",
+            "title": "#1151「怪盗キッドと王冠マジック（後編）」",
+            "broadcast": "1月25日(土)放送分",
+            "end": "2月1日(土)17:59 終了予定"
         }
     }
     TEST_EPISODE_END_DATETIME_REGEX = r"(\d+)月(\d+)日.*?(\d{2}:\d{2})"
